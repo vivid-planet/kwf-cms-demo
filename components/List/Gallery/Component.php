ee51e055a83437229e984332247be51ff2a8b289
Niko Sams <niko.sams@gmail.com>
2013-06-30 12:18:57 +0200

    cleanup: remove custom styling, remove contact component (dynamic form should be used instead)

--- components/List/Gallery/Component.php
+++
@@ -1,9 +0,0 @@
-<?php
-class List_Gallery_Component extends Kwc_List_Gallery_Component
-{
-    public static function getSettings()
-    {
-        $ret = parent::getSettings();
-        return $ret;
-    }
-}
