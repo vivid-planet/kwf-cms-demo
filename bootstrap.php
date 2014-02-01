4d57d3bc525309c69a2065344fe67b5a4e50b721
Niko Sams <niko.sams@gmail.com>
2014-02-01 17:07:27 +0100

    complete update to 3.5

--- bootstrap.php
+++ bootstrap.php
@@ -1,20 +1,10 @@
 <?php
 chdir(dirname(__FILE__));
-$include_path  = get_include_path();
-$include_path .= PATH_SEPARATOR . 'controllers';
-$include_path .= PATH_SEPARATOR . 'models';
-$include_path .= PATH_SEPARATOR . 'components';
-$include_path .= PATH_SEPARATOR . 'app';
-set_include_path($include_path);
 require_once 'kwf-lib/Kwf/Setup.php';
 Kwf_Setup::setUp();
 Kwf_Setup::dispatchKwc();
 Kwf_Setup::dispatchMedia();
-Kwf_Assets_Loader::load();
 
 $front = Kwf_Controller_Front_Component::getInstance();
-
-// acl ist in Acl und in config ist die aclClass gesetzt
-
 $response = $front->dispatch();
 $response->sendResponse();
