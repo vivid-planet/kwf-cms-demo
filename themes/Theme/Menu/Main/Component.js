82bc0a566316fe0e34acd2e1a5f1f45dd7445754
Daniel Bacher <db@vivid-planet.com>
2014-06-16 13:18:27 +0200

    Removed comment

--- themes/Theme/Menu/Main/Component.js
+++ themes/Theme/Menu/Main/Component.js
@@ -1,8 +1,6 @@
 Kwf.Utils.ResponsiveEl('.themeMenuMain', [576]);
 
-/* Kwf.Utils.StickyHeader ?*/
 Kwf.onJElementReady('.themeMenuMain', function(el){
-
     $(window).scroll(function(e){
         var offsetTop = $(document).scrollTop();
         if($(el).offset().top < offsetTop) {
